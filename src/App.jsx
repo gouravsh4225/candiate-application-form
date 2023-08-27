e4680696869a93b43c5c365e21949ed86504c60f
gouravsh4225 <gouravsh4225@gmail.com>
2023-08-27 16:03:40 +0530

    remove routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,8 @@
 import React, { Fragment } from "react";
 import "./App.css";
 import AppRoutes from "./Routers/AppRouters";
-import { createBrowserRouter, RouterProvider, Link } from "react-router-dom";
 
 const App = () => {
-  const router = createBrowserRouter(routes, {
-    basename: import.meta.env.DEV ? "/" : "/candidate-application-form/",
-  });
-
   return (
     <Fragment>
       <AppRoutes />
